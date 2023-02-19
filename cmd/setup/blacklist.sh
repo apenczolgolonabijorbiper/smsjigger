b961b3fdc5967c7ec3826ad25c29a99a49b31805
apenczolgolonabijorbiper <marcin@bibice.pl>
2023-02-19 14:44:18 +0100

    working

---
+++ cmd/setup/blacklist.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IFS=" " read -r recipient blocknumber <<< $(<$2)
+if [[ ! -f ~/sms/cmd/$recipient/generic.sh ]]; then
+	echo "recipient $recipient does not exist"
+else
+	owner=$(<~/sms/cfg/$recipient/owner.cfg)
+	if [[ $1 == $owner ]]; then
+		if [[ ! -f ~/sms/cfg/$recipient/blacklist.lst ]]; then
+			echo "$blocknumber" >> ~/sms/cfg/$recipient/blacklist.lst
+		else
+			check=$(grep -c $blocknumber ~/sms/cfg/$recipient/blacklist.lst)
+			if [[ $check -eq 0 ]]; then
+				timestamp=`date +%s%N`
+				cp ~/sms/cfg/$recipient/blacklist.lst ~/sms/cfg/$recipient/blacklist-old-$timestamp
+				echo "$blocknumber" >> ~/sms/cfg/$recipient/blacklist.lst
+				echo "number $blocknumber blacklisted for recipient $recipient"
+			else
+				echo "number $blocknumber is already blacklisted for recipient $recipient"
+			fi
+		fi
+	else
+		echo "$1 is not owner of $recipient, so $blocknumber cannot be blacklisted"
+	fi
+fi
---
+++ cmd/setup/forward.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IFS=" " read -r recipient forwardnumber <<< $(<$2)
+if [[ ! -f ~/sms/cmd/$recipient/generic.sh ]]; then
+	echo "recipient $recipient does not exist"
+else
+	owner=$(<~/sms/cfg/$recipient/owner.cfg)
+	if [[ $1 == $owner ]]; then
+		if [[ ! -f ~/sms/cfg/$recipient/forward.cfg ]]; then
+			echo "$forwardnumber" >> ~/sms/cfg/$recipient/forward.cfg
+		else
+			check=$(grep -c $forwardnumber ~/sms/cfg/$recipient/forward.cfg)
+			if [[ $check -eq 0 ]]; then
+				timestamp=`date +%s%N`
+				cp ~/sms/cfg/$recipient/forward.cfg ~/sms/cfg/$recipient/forward-old-$timestamp
+				echo "$forwardnumber" >> ~/sms/cfg/$recipient/forward.cfg
+				echo "number $forwardnumber added as forward for recipient $recipient"
+			else
+				echo "number $forwardnumber is already forward for recipient $recipient"
+			fi
+		fi
+	else
+		echo "$1 is not owner of $recipient, so $forwardnumber cannot be set for forward"
+	fi
+fi
